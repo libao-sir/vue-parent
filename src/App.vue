c1e65e38f76f4b0f8d9a7f6a56451b7ce1885616
libao <libao@qq.com>
2021-07-23 13:47:05 +0800

    init registry

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,86 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <a-layout id="components-layout-demo-custom-trigger">
+      <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
+        <div class="logo" />
+        <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
+          <a-menu-item key="1">
+            <router-link to="/">
+              <a-icon type="home" />
+              <span>Home</span>
+            </router-link>
+          </a-menu-item>
+          <a-menu-item key="2">
+            <router-link to="/about">
+              <a-icon type="border" />
+              <span>index</span>
+            </router-link>
+          </a-menu-item>
+          <a-sub-menu key="sub">
+            <span slot="title">
+              <router-link to="/sub/ga">
+                <a-icon type="upload" />Sub
+              </router-link>
+            </span>
+            <a-menu-item key="3">
+              <router-link to="/sub/ga">
+                <a-icon type="google" />
+                <span>GA</span>
+              </router-link>
+            </a-menu-item>
+            <a-menu-item key="4">
+              <router-link to="/sub">
+                <a-icon type="yuque" />
+                <span>ERP</span>
+              </router-link>
+            </a-menu-item>
+          </a-sub-menu>          
+        </a-menu>
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header style="background: #fff; padding: 0">
+          <a-icon class="trigger" :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+            @click="() => (collapsed = !collapsed)" />
+        </a-layout-header>
+        <a-layout-content :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }">
+          <router-view></router-view>
+          <div id="sub"></div>
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
   </div>
 </template>
-
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        collapsed: false,
+      };
+    },
+  };
+</script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  .ant-layout {
+    height: 100vh;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #components-layout-demo-custom-trigger .trigger {
+    font-size: 18px;
+    line-height: 64px;
+    padding: 0 24px;
+    cursor: pointer;
+    transition: color 0.3s;
+  }
+
+  #components-layout-demo-custom-trigger .trigger:hover {
+    color: #1890ff;
+  }
+
+  #components-layout-demo-custom-trigger .logo {
+    height: 32px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 16px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
+import { registerMicroApps, start} from 'qiankun'
 
 Vue.config.productionTip = false
+
+
+Vue.use(Antd)
 
 new Vue({
   router,
   render: function (h) { return h(App) }
 }).$mount('#app')
+
+registerMicroApps([
+  {
+    name: 'sub',
+    entry: '//localhost:8081',
+    container: '#sub',
+    activeRule: '/sub',
+  }
+]);
+
+start();
